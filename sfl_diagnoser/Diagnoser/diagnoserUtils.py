01331fe01f5e67c96158777f8f6829acb215fe9a
amir9979 <amir9979@gmail.com>
2019-03-04 18:26:42 +0200

    Update diagnoserUtils.py

--- sfl_diagnoser/Diagnoser/diagnoserUtils.py
+++ sfl_diagnoser/Diagnoser/diagnoserUtils.py
@@ -31,7 +31,10 @@ def readPlanningFile(fileName, delimiter=";"):
     priors=eval(priorsStr[0])
     bugs=eval(BugsStr[0])
     initials=eval(InitialsStr[0])
-    components = dict(eval(eval(components_names[0].replace(delimiter, ','))))
+    try:
+        components = dict(eval(components_names[0].replace(delimiter, ',')))
+    except:
+        components = dict(eval(eval(components_names[0].replace(delimiter, ','))))
     testsPool={}
     estimatedTestsPool = {}
     error={}
